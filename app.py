2d16853492d29193b95d774c8cdae4c17cd35ef2
ThatsSid <siddharthbelbase@gmail.com>
2020-07-03 01:09:03 +0545

    changed from polling to webhook

--- app.py
+++ app.py
@@ -78,11 +78,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
 
-    # updater.bot.setWebhook('https://covidnepal-bot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://covidnepal-bot.herokuapp.com/' + TOKEN)
 
-    updater.start_polling()
+    # updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
